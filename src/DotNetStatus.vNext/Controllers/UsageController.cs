3e481ce3b99ea49bb7de949cec8646376ad621a3
Taylor Southwick <tasou@microsoft.com>
2014-11-13 13:34:17 -0800

    Add some controllers to vNext

---
+++ src/DotNetStatus.vNext/Controllers/UsageController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Fx.Portability;
+using Microsoft.Fx.Portability.ObjectModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotNetStatus.Controllers
+{
+    public class UsageController : Controller
+    {
+        private readonly IApiPortService _apiPortService;
+
+        public UsageController(IApiPortService apiPortService)
+        {
+            _apiPortService = apiPortService;
+        }
+
+        [Route("usage")]
+        public async Task<ActionResult> Index()
+        {
+            return View(await GetViewModelAsync(0, 25, UsageDataFilter.HideSupported));
+        }
+
+        [Route("usage/page")]
+        public async Task<PartialViewResult> Page(int? skip, int? top, UsageDataFilter? filter, IEnumerable<string> targets)
+        {
+            return PartialView("_Usage", await GetViewModelAsync(skip ?? 0, top, filter, targets));
+        }
+
+        private async Task<UsageDataCollection> GetViewModelAsync(int skip, int? top = null, UsageDataFilter? filter = null, IEnumerable<string> targets = null)
+        {
+            try
+            {
+                var usage = await _apiPortService.GetUsageDataAsync(skip, top, filter, targets);
+
+                return usage.Response;
+            }
+            catch (PortabilityAnalyzerException)
+            {
+                return null;
+            }
+        }
+    }
+}
--- src/DotNetStatus.vNext/Startup.cs
+++ src/DotNetStatus.vNext/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
 
 namespace DotNetStatus.vNext
 {
@@ -8,7 +9,12 @@ namespace DotNetStatus.vNext
     {
         public void Configure(IApplicationBuilder app)
         {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+            app.UseMvc();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
         }
     }
 }

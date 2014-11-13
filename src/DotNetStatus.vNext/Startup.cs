255f8fc5846f274d372127ab10fdc362044fc21e
Taylor Southwick <tasou@microsoft.com>
2014-11-13 15:21:32 -0800

    Add bower, grunt, and get home working. Set grunt.js to execute tasks before build.

--- src/DotNetStatus.vNext/Startup.cs
+++ src/DotNetStatus.vNext/Startup.cs
@@ -2,18 +2,51 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.Logging;
+using Microsoft.Framework.Logging.Console;
+using Microsoft.AspNet.Diagnostics;
+using Microsoft.Framework.ConfigurationModel;
 
 namespace DotNetStatus.vNext
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public Startup(IHostingEnvironment env)
         {
+            Configuration = new Configuration()
+                .AddEnvironmentVariables();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            
+            // Add the following to the request pipeline only in development environment.
+            if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
+            {
+                app.UseBrowserLink();
+                app.UseErrorPage(ErrorPageOptions.ShowAll);
+            }
+            else
+            {
+                // Add Error handling middleware which catches all application specific errors and
+                // send the request to the following path or controller action.
+                app.UseErrorHandler("/Home/Error");
+            }
+
+            // Add static files to the request pipeline
+            app.UseStaticFiles();
+
+            // Add MVC to the request pipeline (we use attribute routing)
             app.UseMvc();
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add MVC services to container
             services.AddMvc();
         }
     }

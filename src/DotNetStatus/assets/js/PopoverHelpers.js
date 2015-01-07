9097957dce36b1d71f3e6a397c2c37bceb46cdbc
Connie Yau <conniey@microsoft.com>
2015-01-07 00:56:18 -0800

    Initial commit from mseng/master. Keeping only relevant components that are used in the website.

---
+++ src/DotNetStatus/assets/js/PopoverHelpers.js
@@ -0,0 +1,16 @@
+﻿function InitializePopovers() {
+    $(".popup-marker").popover();
+    $(".popup-marker").on('click', function (e) { e.preventDefault(); return true; });
+
+    $('.popup-marker').popover();
+
+    $('body').on('click', function (e) {
+        $('.popup-marker').each(function () {
+            if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover-marker').has(e.target).length === 0) {
+                $(this).popover('hide');
+            }
+        });
+    });
+}
+
+$(document).ready(InitializePopovers);
---
+++ src/DotNetStatus/assets/js/Search.js
@@ -0,0 +1,93 @@
+﻿$(document).ready(function () {
+    var $searchTextBox = $("#searchTextBox");
+    var $searchButton = $("#searchButton");
+    var $hiddenDocId = $("#docId");
+
+    function DisableButton() {
+        $searchButton.attr('disabled', 'disabled');
+    }
+
+    function EnableButton() {
+        $searchButton.removeAttr('disabled');
+    }
+
+    function InvalidTextBox() {
+        $searchTextBox.addClass('invalid-query');
+    }
+
+    function ValidTextBox() {
+        $searchTextBox.removeClass('invalid-query');
+    }
+
+    DisableButton();
+
+    function ValidateTextBox(url) {
+        var query = $searchTextBox.val();
+
+        // If search box is empty, invalid mark (if applied)
+        if (query == '') {
+            ValidTextBox();
+        } else {
+            // Call API to see if search box contains valid query
+            $.ajax({
+                type: "GET",
+                url: url,
+                data: "query=" + query,
+                error: function (err) {
+                    InvalidTextBox();
+                    DisableButton();
+                },
+                success: function (output) {
+                    ValidTextBox();
+                    EnableButton();
+                    $hiddenDocId.val(output.DocId);
+                }
+            });
+        }
+    }
+
+    var autocompleteUrl = $searchTextBox.data("autocomplete-url");
+    var validateUrl = $searchTextBox.data("autocomplete-validate-url");
+
+    $searchTextBox.change(function () {
+        ValidateTextBox(validateUrl)
+    });
+
+    $searchTextBox.keyup(function (e) {
+        // Check if ESC was pressed
+        if (e.keyCode == 27) {
+            $searchTextBox.val("");
+        }
+
+        ValidateTextBox(validateUrl);
+    });
+
+    // Initialize Bootstrap autocomplete
+    $searchTextBox.autocomplete({
+        source: autocompleteUrl,
+        focus: function (event, ui) {
+            $searchTextBox.val(ui.item.FullName);
+            $hiddenDocId.val(ui.item.DocId);
+
+            ValidateTextBox(validateUrl);
+
+            return false;
+        },
+        select: function (event, ui) {
+            $hiddenDocId.val(ui.item.DocId);
+            $searchTextBox.val(ui.item.FullName);
+
+            ValidateTextBox(validateUrl);
+
+            $searchButton.click();
+
+            return true;
+        }
+    })
+    // Display only full name from results data
+    .autocomplete("instance")._renderItem = function (ul, item) {
+        return $("<li>")
+            .append("<a>" + item.FullName + "</a>")
+            .appendTo(ul);
+    };
+});

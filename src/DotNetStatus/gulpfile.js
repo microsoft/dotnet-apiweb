58da4d7d28482505c87742e4489af1bf8e5fcd0f
Taylor Southwick <tasou@microsoft.com>
2015-07-20 13:29:59 -0700

    Fix broken css and js

--- src/DotNetStatus/gulpfile.js
+++ src/DotNetStatus/gulpfile.js
@@ -17,6 +17,8 @@ paths.css = "assets/css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.bower = "bower_components/";
+paths.lib = paths.webroot + "lib/";
 
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
@@ -42,4 +44,20 @@ gulp.task("min:css", function () {
         .pipe(gulp.dest("."));
 });
 
+gulp.task("copy", function () {
+    var bower = {
+        "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,eot}",
+        "jquery": "jquery/dist/jquery*.{js,map}",
+        "jquery-ui": "jquery-ui/jquery-ui.js",
+        "jquery-ui-smoothness": "jquery-ui-smoothness/jquery-ui.css"
+    };
+
+    for (var module in bower) {
+        gulp.src(paths.bower + bower[module])
+          .pipe(gulp.dest(paths.lib + module));
+    };
+});
+
 gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("build", ["copy", "min"]);
